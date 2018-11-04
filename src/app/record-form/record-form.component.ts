5a65c5f181f804374d21e64ddc80afcb96297ac4
vladlenkaraush <vladlenkaraush@gmail.com>
2018-11-04 14:20:19 +0300

    changed forms to bootstrap forms, added post request

--- src/app/record-form/record-form.component.ts
+++ src/app/record-form/record-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-record-form',
@@ -11,32 +14,40 @@ export class RecordFormComponent implements OnInit {
   latitudeDirections = [];
   longitudeDirections = [];
   record;
-  constructor() { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
     this.latitudeDirections = ['S', 'N'];
     this.longitudeDirections = ['W', 'E'];
 
-    this.record = new FormGroup({
-      latitude : new FormGroup({
-        degrees: new FormControl(''),
-        minutes: new FormControl(''),
-        seconds: new FormControl(''),
-        direction: new FormControl(''),
+    this.record = this.fb.group({
+      latitude : this.fb.group({
+        degrees: ['', Validators.compose([Validators.required, Validators.min(-180), Validators.max(180)])],
+        minutes: ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(60)])],
+        seconds: ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(60)])],
+        direction: [''],
       }),
-      longitude : new FormGroup({
-        degrees: new FormControl(''),
-        minutes: new FormControl(''),
-        seconds : new FormControl(''),
-        direction: new FormControl(''),
+      longitude : this.fb.group({
+        degrees: ['', Validators.compose([Validators.required, Validators.min(-180), Validators.max(180)])],
+        minutes: ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(60)])],
+        seconds : ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(60)])],
+        direction: [''],
       }),
-      temperature : new FormControl('')
+      temperature : ['', Validators.compose([Validators.required, Validators.min(-100), Validators.max(100)])]
     });
   }
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value
     console.warn(this.record.value);
+    const val = this.record.value;
+    const recordDTO = {
+      latitude: val.latitude.degrees + '° ' + val.latitude.minutes + '′ ' + val.latitude.seconds + '″ ' + val.latitude.direction,
+      longitude: val.longitude.degrees + '° ' + val.longitude.minutes + '′ ' + val.longitude.seconds + '″ ' + val.longitude.direction,
+      temperature: val.temperature
+    };
+    console.warn(recordDTO);
+    const ans = this.http.post('http://localhost:8080/records', recordDTO);
+
   }
 
 }
